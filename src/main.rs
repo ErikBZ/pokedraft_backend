cb666ee24069693c3ed9734162351bdd3c5f7f9f
Brandon Zapata <zapatabrandon@gmail.com>
2024-01-24 22:12:06 -0500

    Add initial test code for connecting to surreal db

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,40 @@
 #[macro_use] extern crate rocket;
+use serde::{Deserialize, Serialize};
+use surrealdb::engine::remote::ws::{self, Ws, Client};
+use surrealdb::opt::auth::Root;
+use surrealdb::sql::Thing;
+use surrealdb::{Error, Surreal};
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+async fn init_db() -> Surreal<Client> {
+    let db = match Surreal::new::<Ws>("127.0.0.1:8000").await {
+        Err(_) => panic!("Unable to start connection to DB"),
+        Ok(f) => f,
+    };
+
+    match db.signin(Root {username: "root", password: "root"}).await {
+        Err(_) => panic!("Unable to login to DB"),
+        Ok(_) => (),
+    }
+
+    match db.use_ns("test").use_db("test").await {
+        Err(_) => panic!("Unable to start namespace or database"),
+        Ok(_) => (),
+    }
+
+    db
+}
+
 #[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+async fn rocket() -> _ {
+    let db = init_db().await;
+
+    // Connect to the server
+    rocket::build()
+        .manage(db)
+        .mount("/", routes![index])
 }
