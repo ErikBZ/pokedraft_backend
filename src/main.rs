7261080ffaf52e0b6742e998b11c3c8423b0b367
Brandon Zapata <zapatabrandon@gmail.com>
2024-01-27 13:09:04 -0500

    test commit

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,64 @@
 #[macro_use] extern crate rocket;
+use std::string;
+
 use serde::{Deserialize, Serialize};
 use surrealdb::engine::remote::ws::{self, Ws, Client};
 use surrealdb::opt::auth::Root;
 use surrealdb::sql::Thing;
 use surrealdb::{Error, Surreal};
+use rocket::{State};
+
+#[derive(Debug, Serialize)]
+enum PokemonType {
+    NORMAL,
+    FIRE,
+    WATER,
+    ELECTRIC,
+    GRASS,
+    ICE,
+    FIGHTING,
+    POISON,
+    GROUND,
+    FLYING,
+    PSYCHIC,
+    BUG,
+    ROCK,
+    GHOST,
+    DRAGON,
+    DARK,
+    STEEL,
+    FAIRY,
+}
+
+#[derive(Debug, Serialize)]
+struct Pokemon <'a>{
+    name: &'a str,
+    type1: PokemonType,
+    type2: PokemonType,
+    evolves_from: &'a str,
+    gen: u8,
+    is_legendary: bool,
+    is_mythic: bool,
+}
+
+#[derive(Debug, Deserialize)]
+struct Record {
+    id: Thing
+}
+
 
 #[get("/")]
-fn index() -> &'static str {
+async fn index(db: &State<Surreal<Client>>) -> &'static str {
+    let created: Result<Vec<Record>, Error> = db.create("pokemon").content( Pokemon {
+        name: "Bulbasaur",
+        type1: PokemonType::GRASS,
+        type2: PokemonType::POISON,
+        evolves_from: "0",
+        gen: 1,
+        is_legendary: false,
+        is_mythic: false
+    }).await;
+
     "Hello, world!"
 }
 
